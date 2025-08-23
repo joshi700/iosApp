ae83e10497c79c2d9a2a4a18e3cafcbf428e529a
Gaurav Joshi <gauravjoshi@Gauravs-Mac-mini.local>
2025-08-23 22:06:12 +0530

    Your commit message

--- api/checkorder.js
+++ api/checkorder.js
@@ -1,3 +1,48 @@
+// const axios = require('axios');
+// require('dotenv').config();
+
+// // Serverless function handler for Vercel
+// module.exports = async (req, res) => {
+//   // Enable CORS
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+//   // Handle preflight OPTIONS request
+//   if (req.method === 'OPTIONS') {
+//     return res.status(200).end();
+//   }
+
+//   // Only allow GET method
+//   if (req.method !== 'GET') {
+//     return res.status(405).json({ error: 'Method not allowed' });
+//   }
+
+//   const orderId = req.query.orderid;
+
+//   if (!orderId) {
+//     return res.status(400).json({ error: 'Missing orderid parameter' });
+//   }
+
+//   try {
+//     const config = {
+//       method: 'get',
+//       maxBodyLength: Infinity,
+//       url: `https://mtf.gateway.mastercard.com/api/rest/version/llaatteesstt/merchant/TESTMIDtesting00/order/${orderId}`,
+//       headers: { 
+//         'Authorization': process.env.PASSWORD
+//       }
+//     };
+
+//     const response = await axios.request(config);
+//     console.log(JSON.stringify(response.data));
+//     return res.status(200).json(response.data);
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ error: 'Failed to fetch order information' });
+//   }
+// };
+
 const axios = require('axios');
 require('dotenv').config();
 
@@ -30,7 +75,7 @@ module.exports = async (req, res) => {
       maxBodyLength: Infinity,
       url: `https://mtf.gateway.mastercard.com/api/rest/version/llaatteesstt/merchant/TESTMIDtesting00/order/${orderId}`,
       headers: { 
-        'Authorization': process.env.PASSWORD
+        'Authorization': `Basic ${process.env.PASSWORD}`
       }
     };
 
--- api/checkout.js
+++ api/checkout.js
@@ -1,3 +1,70 @@
+// const axios = require('axios');
+// const crypto = require("crypto");
+// require('dotenv').config();
+
+// // Serverless function handler for Vercel
+// module.exports = async (req, res) => {
+//   // Enable CORS
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+//   // Handle preflight OPTIONS request
+//   if (req.method === 'OPTIONS') {
+//     return res.status(200).end();
+//   }
+
+//   // Only allow POST method
+//   if (req.method !== 'POST') {
+//     return res.status(405).json({ error: 'Method not allowed' });
+//   }
+
+//   try {
+//     const id = crypto.randomBytes(16).toString("hex");
+//     const data = JSON.stringify({
+//       "apiOperation": "INITIATE_CHECKOUT",
+//       "checkoutMode": "WEBSITE",
+//       "interaction": {
+//         "operation": "PURCHASE",
+//         "merchant": {
+//           "name": "JK Enterprises LLC",
+//           "url": "https://apitest.free.beeceptor.com"
+//         },
+//         "returnUrl": "myapp://receipt",
+//         "displayControl": {
+//           "billingAddress": "READ_ONLY",
+//           "customerEmail": "READ_ONLY",
+//           "shipping": "READ_ONLY"
+//         }
+//       },
+//       "order": {
+//         "currency": "USD",
+//         "amount": "250.00",
+//         "id": id,
+//         "description": "Goods and Services"
+//       }
+//     });
+
+//     const config = {
+//       method: 'post',
+//       maxBodyLength: Infinity,
+//       url: 'https://mtf.gateway.mastercard.com/api/rest/version/llaatteesstt/merchant/TESTMIDtesting00/session',
+//       headers: { 
+//         'Content-Type': 'application/json', 
+//         'Authorization': process.env.PASSWORD
+//       },
+//       data
+//     };
+
+//     const response1 = await axios.request(config);
+//     console.log(JSON.stringify(response1.data.session.id));
+//     res.status(200).send(response1.data.session.id);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Failed to initiate checkout session' });
+//   }
+// };
+
 const axios = require('axios');
 const crypto = require("crypto");
 require('dotenv').config();
@@ -51,7 +118,7 @@ module.exports = async (req, res) => {
       url: 'https://mtf.gateway.mastercard.com/api/rest/version/llaatteesstt/merchant/TESTMIDtesting00/session',
       headers: { 
         'Content-Type': 'application/json', 
-        'Authorization': process.env.PASSWORD
+        'Authorization': `Basic ${process.env.PASSWORD}`
       },
       data
     };
